a154d3e9208c1a048e4df38b3640295bbc80fd3f
Fola <folajimibabslawal@gmail.com>
2026-05-16 08:59:30 +0100

    leetcode

--- LongestUniqueSubstring.java
+++ LongestUniqueSubstring.java
@@ -1,9 +1,10 @@
 public class LongestUniqueSubstring {
 
     static void main() {
-        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
     }
     public static int lengthOfLongestSubstring(String s){
+        int largest = 1;
         StringBuilder builder = new StringBuilder();
         builder.append(s.substring(0,1));
         System.out.println(builder);
@@ -11,9 +12,13 @@ public class LongestUniqueSubstring {
             char c = s.charAt(count + 1);
             if(!builder.toString().contains(String.valueOf(c))){
                 builder.append(c);
+                if(builder.length() > largest) largest = builder.length();
+            }else {
+                builder = new StringBuilder();
+                builder.append(s.substring(count, count + 1));
             }
 
         }
-        return builder.length();
+        return largest;
     }
 }
